6389f68a754451c525ceccfe98a0a38e9e2d3d97
Paul Levitsky <pasha.account@gmail.com>
2022-09-09 14:11:21 +0200

    initial

---
+++ src/Components/MyNav.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import {Component} from "react";
+
+
+class MyNav extends Component{
+    state={
+        data: ""
+    }
+    render(){
+        return(
+        <>
+        </>
+    )
+}
+}
+export default MyNav
